d5ac68564eefe227ed17ee97abedab26a71f88ad
The Fanatic <don.manula@gmail.com>
2020-08-22 20:24:00 +0530

    drivers added

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Demo {
 
     public static void main(String[] args){
+// invoking browser
+        System.setProperty("webdriver.chrome.driver","D:\\Projects\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");
+
+
 
     }
 }
